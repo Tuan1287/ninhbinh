b7ad0b8fe3ce1b088a2ed772f71cdfbe293c62c4
Tuan1287 <formylove128@gmail.com>
2026-04-02 16:11:52 +0700

    Update nhahang.js

--- data/nhahang.js
+++ data/nhahang.js
@@ -1,1 +1,44 @@
+const nhahang = [
+  {
+    id: 1,
+    ten: "Nhà hàng Thăng Long",
+    mon_noi_bat: ["cơm cháy", "dê núi"],
+    gia_trung_binh: 150000,
+    khu_vuc: "Tràng An",
+    ghi_chu: "Phục vụ đoàn khách lớn"
+  },
+  {
+    id: 2,
+    ten: "Nhà hàng Đức Dê",
+    mon_noi_bat: ["dê tái chanh", "dê nướng"],
+    gia_trung_binh: 200000,
+    khu_vuc: "Hoa Lư",
+    ghi_chu: "Đặc sản dê nổi tiếng"
+  },
+  {
+    id: 3,
+    ten: "Family Restaurant Tam Cốc",
+    mon_noi_bat: ["đồ Việt", "pizza"],
+    gia_trung_binh: 120000,
+    khu_vuc: "Tam Cốc",
+    ghi_chu: "Phù hợp khách quốc tế"
+  },
+  {
+    id: 4,
+    ten: "Chookie's Beer Garden",
+    mon_noi_bat: ["BBQ", "bia"],
+    gia_trung_binh: 180000,
+    khu_vuc: "Tam Cốc",
+    ghi_chu: "Ăn tối chill"
+  },
+  {
+    id: 5,
+    ten: "Nhà hàng Hoàng Giang",
+    mon_noi_bat: ["cơm cháy", "lẩu dê"],
+    gia_trung_binh: 130000,
+    khu_vuc: "Trung tâm",
+    ghi_chu: "Giá hợp lý"
+  }
+];
 
+export default nhahang;
